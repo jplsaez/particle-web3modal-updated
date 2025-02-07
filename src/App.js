cbf8828194f4e2890a1293a34754b72123dc0142
Juan Pedro Lopez Saez <jplsaez@gmail.com>
2025-02-07 06:38:31 +0100

    Fixes

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import ParticleProvider from './particle';
-function App({ children }) {
-    return (_jsx(ParticleProvider, { children: children }));
-}
-export default App;
--- src/connectButton/index.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export default function ConnectButton() {
-    return _jsx("w3m-button", {});
-}
